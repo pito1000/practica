a9f4fc2779b985f3a4d7e15757244eafec71abde
unknown <Alumno@SLCID13.svcs.itesm.mx>
2020-01-28 18:38:01 -0600

    sumar

---
+++ putoelquienlolea.py
@@ -0,0 +1,7 @@
+print ("hola")
+numero1 = input ("introdusca el primer numero  :")
+numero2 = input ("introdusca el segundo numero  :")
+
+suma = int(numero1) + int(numero2)
+
+print ("el resultado de la suma de tus dos numeros es:"  +  str(suma))
